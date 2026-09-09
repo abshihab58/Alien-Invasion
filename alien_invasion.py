a82e17dbb622167d56d9af716857c21c2f91f8e8
abshihab58 <abubakarshihab118@gmail.com>
2026-09-10 00:58:49 +0600

    Updated project files

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,18 +27,34 @@ class AlienInvasion:
     def run_game(self):
         
         while True:
-        
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            #redraw the screen during each pass through the loop.        
+            self._check_events()
+            self._update_screen()
+              
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
             self.clock.tick(60)  # Limit the frame rate to 60 FPS
+
+    def _check_events(self):
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+
+                    self.ship.rect.x += 1
+
+    def _update_screen(self):
+        
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
 if __name__ == '__main__':
     # Make a game instance, and run the game.
     ai = AlienInvasion()
