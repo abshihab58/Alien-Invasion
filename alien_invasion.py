ee3f20539c3a07c8c6938d17b4fbf2d9a977acda
abshihab58 <abubakarshihab118@gmail.com>
2026-09-09 16:04:31 +0600

    update alien_invasion

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,31 +1,40 @@
 import sys
+
 import pygame
+
 from settings import Settings
+
+from ship import Ship
+
 class AlienInvasion:
     
-    """Overall class to manage game assets and behavior."""
-
     def __init__(self):
         
-        """Initialize the game, and create game resources."""
         pygame.init()
+
         self.clock = pygame.time.Clock()
         self.settings = Settings()
+
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        # self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
         #set the background color
-        self.bg_color = (230, 230, 230)
+        # self.bg_color = (230, 230, 230)
 
     def run_game(self):
-        """Start the main loop for the game."""
+        
         while True:
-            # Watch for keyboard and mouse events.
+        
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
             #redraw the screen during each pass through the loop.        
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
@@ -35,5 +44,3 @@ if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
 
-   
-   
