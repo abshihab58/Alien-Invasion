98dd01e0c75f40f61acd2adc8e27c617a2e65f46
abshihab58 <abubakarshihab118@gmail.com>
2026-09-24 00:25:48 +0600

    update website

--- alien_invasion.py
+++ alien_invasion.py
@@ -118,8 +118,10 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
         if collisions:
-            self.stats.score += self.settings.alien_points
-            self.sb.prep_score()
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+                self.sb.prep_score()
+                self.sb.check_high_score()  # Check if the current score is a new high score
 
         if not self.aliens:
             self.bullets.empty()
